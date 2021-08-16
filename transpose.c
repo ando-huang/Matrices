8b4d9902f2f231f658938d54325ac76aef4123d0
ando-huang <ahuang09182000@gmail.com>
2021-08-16 12:42:37 -0700

    implemented transpose in C

--- transpose.c
+++ transpose.c
@@ -5,7 +5,7 @@ int* generateMatrix(int n){
     int* matrix = (int*)malloc(sizeof(int) * n * n);
     int i, j;
     for(i = 0; i < n; i++){
-        for(j = i; j < n; j++){
+        for(j = 0; j < n; j++){
             matrix[i*n+j] = (100 * i) + j;
         }
     }
@@ -16,20 +16,39 @@ void printMatrix(int* matrix, int n){
     int i, j;
     for(i = 0; i < n; i++){
         for(j = 0; j < n; j++){
-            printf("%d ", matrix[(n * i) + j]);
+            printf("%3d ", matrix[n*i+j]);
         }
         printf("\n");
     }
 }
 
+void transposeMatrix(int* matrix, int n){
+    int i, j;
+    for(i = 0; i < n; i++){
+        for(j = i; j < n; j++){
+            int temp = matrix[n*i+j];
+            matrix[n*i+j] = matrix[n*j+i];
+            matrix[n*j+i] = temp;
+        }
+    }
+}
+
 int main(){
     int n = -1;
-    while(n < 1){
-        scanf("Welcome to transpose.c, please enter a value %d\n", &n);
-        printf("you entered %d", n);
+    printf("Welcome to transpose.c, please enter a matrix size n:\n");
+    scanf("%d", &n);
+    while(n < 2){
+        printf("Please enter a value greater than 1 to see the full item \n");
+        scanf("%d", &n);
     }
     int* matrix = generateMatrix(n);
+    printMatrix(matrix, n); 
     
+    printf("now transposing that matrix\n");
+
+    transposeMatrix(matrix, n);
+    printMatrix(matrix, n);
+
     free(matrix);
     matrix = NULL;
     return 0;
